1e1a01ed9d26cd5b072f1de72ee47a7789157f7c
CeNfy0 <157808989+CeNfy0@users.noreply.github.com>
2024-11-02 10:11:40 +0000

    Fix index

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,11 @@ import './index.css';
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
-import ContentScreen from './ContentScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContentScreen />
+    <App />
   </React.StrictMode>
 );
 
