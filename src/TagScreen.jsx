9ba5889a2f1d576bd7c1c62fda4241e93a787cbd
CeNfy0 <157808989+CeNfy0@users.noreply.github.com>
2024-11-03 17:45:51 +0000

    delHeader

--- src/TagScreen.jsx
+++ src/TagScreen.jsx
@@ -14,24 +14,6 @@ function TagScreen() {
 
   return (
     <div className="App">
-      <div className="head">
-        <img className="pointer" style={{ marginLeft: 30 }} align="left" src="src/logo.png" width={100} alt="logo" />
-        <div className="search-container">
-          <input
-            className="search-input"
-            type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            placeholder="ค้นหา"
-          />
-          <FaSearch
-            onClick={() => alert("TRY")}
-            className="search-icon"
-          />
-        </div>
-        <button className="log-button pointer">เข้าสู่ระบบ / Log In</button>
-      </div>
-
       <main className="main-content">
         <div className="tags-header">
           <div className="profile-header"> 
