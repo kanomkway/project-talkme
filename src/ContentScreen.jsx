cfbf6c02aa5f4d3ccd46f5746fa33e709bb665e2
kanomkway <jenny.yatika@gmail.com>
2024-11-06 06:33:00 +0700

    delete_footer

--- src/ContentScreen.jsx
+++ src/ContentScreen.jsx
@@ -1,4 +1,4 @@
-import './ContentScreen.css';
+import "./ContentScreen.css";
 import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import {
@@ -9,51 +9,48 @@ import {
   Link,
   useLocation,
 } from "react-router-dom";
-import Swal from 'sweetalert2';
-import axios from 'axios';
+import Swal from "sweetalert2";
+import axios from "axios";
 
 function ContentScreen() {
-
   const apiurl = "http://localhost:5000";
   const location = useLocation();
-  const {id,title,cont,content,tag} = location.state || {};
+  const { id, title, cont, content, tag } = location.state || {};
 
-  const [comment,setComment] = useState([]);
+  const [comment, setComment] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!id || !tag) {
       console.log("Missing id or tag");
       return;
     }
     axios
       .get(`${apiurl}/content?id=${id}&tag=${tag}`)
-    .then(response=>{
-      setComment(response.data);
-      console.log(response.data);
-      if(!id || !tag || !info){
-        console.log("Sending:", {
-          id: id,
-          tag:tag,
-          info:info
-        });   
-      }
-      
-    })
-    .catch(error=>{
-      console.log("error!");
-    })
-    return ()=>{
-    }}
-    ,[])
+      .then((response) => {
+        setComment(response.data);
+        console.log(response.data);
+        if (!id || !tag || !info) {
+          console.log("Sending:", {
+            id: id,
+            tag: tag,
+            info: info,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("error!");
+      });
+    return () => {};
+  }, []);
 
-  const [info,setInfo] = useState("")
+  const [info, setInfo] = useState("");
 
   const handleInfo = (e) => {
     setInfo(e.target.value);
   };
 
   const insertComment = async () => {
-    console.log(id, info,tag);
+    console.log(id, info, tag);
     if (!id || !info || !tag) {
       Swal.fire({
         title: "ผิดพลาด!",
@@ -65,13 +62,13 @@ function ContentScreen() {
     }
     console.log("Sending:", {
       id: id,
-      tag:tag,
-      info:info
+      tag: tag,
+      info: info,
     });
     try {
       const responsedata = await axios.post(`${apiurl}/api/addcomment`, {
-        id:id,
-        tag:tag,
+        id: id,
+        tag: tag,
         info: info,
       });
       const data = responsedata.data;
@@ -99,36 +96,31 @@ function ContentScreen() {
 
   const commentList = comment?.map((item, index) => (
     <div className="comment-container" key={index}>
-      <h4>Comment #{index+1}</h4>
-      <p className="comment-content">
-        {item.info}
-      </p>
+      <h4>Comment #{index + 1}</h4>
+      <p className="comment-content">{item.info}</p>
     </div>
-));
+  ));
 
   return (
     <main className="main-content">
-    <div className="post-content-container">
-      <h3 className="post-title">{title}a</h3>
-      <p className="post-content">
-        {cont}
-      </p>
-      <div className="post-footer">
-        <button className="like-button"></button>
-        <button className="share-button">เล็งหัวอิงหู</button>
+      <div className="post-content-container">
+        <h3 className="post-title">{title}a</h3>
+        <p className="post-content">{cont}</p>
       </div>
-    </div>
-    {commentList}
-    <div className="comment-input-container">
-      <label htmlFor="comment">Comment :</label>
-      <textarea id="comment" className="comment-input" placeholder="Write a comment..." 
-      onChange={handleInfo}></textarea>
-      <button className="send-button"
-      onClick={()=>insertComment()}
-      >
-        SEND</button>
-    </div>
-  </main>
+      {commentList}
+      <div className="comment-input-container">
+        <label htmlFor="comment">Comment :</label>
+        <textarea
+          id="comment"
+          className="comment-input"
+          placeholder="Write a comment..."
+          onChange={handleInfo}
+        ></textarea>
+        <button className="send-button" onClick={() => insertComment()}>
+          SEND
+        </button>
+      </div>
+    </main>
   );
 }
 
